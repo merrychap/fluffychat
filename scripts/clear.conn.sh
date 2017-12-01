fe543a597551b26d459209b100cb3dc9f145f8c6
merrychap <merrychap.c@gmail.com>
2017-12-01 18:47:07 +0500

    added scripts

---
+++ scripts/clear.conn.sh
@@ -0,0 +1,3 @@
+rm ../database.db
+rm ../app.log
+python3 main.py --recv-port 8080 --port 8080 --host 10.113.226.38
--- scripts/clear.run.sh
+++ scripts/clear.run.sh
@@ -1,3 +1,3 @@
-rm database.db
-rm app.log
+rm ../database.db
+rm ../app.log
 python3 main.py --recv-port 8080
